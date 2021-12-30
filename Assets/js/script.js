b7e5f926a3417be453e0be0ad378b717f2c7aece
Alan Phelps <phelpsa64@gmail.com>
2021-12-29 20:55:05 -0500

    tasks for each hour save to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,3 @@
-var tasks = {};
-
 var currentDate = function() {
     var date = moment().format("dddd, MMMM Do");
 
@@ -9,6 +7,95 @@ var currentDate = function() {
 }
 
 var saveTaskOne = function() {
-    var text = $('#taskTextOne').val();
+    var text = $('#taskTextOne')
+    .val()
+    .trim();
+
+    localStorage.setItem('task1', JSON.stringify(text));
+
     console.log(text);
+}
+
+var saveTaskTwo = function() {
+    var text = $('#taskTextTwo')
+    .val()
+    .trim();
+
+    localStorage.setItem('task2', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskThree = function() {
+    var text = $('#taskTextThree')
+    .val()
+    .trim();
+
+    localStorage.setItem('task3', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskFour = function() {
+    var text = $('#taskTextFour')
+    .val()
+    .trim();
+
+    localStorage.setItem('task4', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskFive = function() {
+    var text = $('#taskTextFive')
+    .val()
+    .trim();
+
+    localStorage.setItem('task5', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskSix = function() {
+    var text = $('#taskTextSix')
+    .val()
+    .trim();
+
+    localStorage.setItem('task6', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskSeven = function() {
+    var text = $('#taskTextSeven')
+    .val()
+    .trim();
+
+    localStorage.setItem('task7', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskEight = function() {
+    var text = $('#taskTextEight')
+    .val()
+    .trim();
+
+    localStorage.setItem('task8', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var saveTaskNine = function() {
+    var text = $('#taskTextNine')
+    .val()
+    .trim();
+
+    localStorage.setItem('task9', JSON.stringify(text));
+
+    console.log(text);
+}
+
+var loadTaskOne = function() {
+    $('#taskTextOne').textContent = JSON.parse(localStorage.getItem('task1'));
 }
