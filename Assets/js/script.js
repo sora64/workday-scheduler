120ffc99bbbee2a08dfaad96f0475e654c4973e9
Alan Phelps <phelpsa64@gmail.com>
2021-12-29 20:26:58 -0500

    found value to save in local storage for tasks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,14 @@
+var tasks = {};
+
 var currentDate = function() {
     var date = moment().format("dddd, MMMM Do");
 
     console.log(date);
 
     $('#currentDay').append(date);
+}
+
+var saveTaskOne = function() {
+    var text = $('#taskTextOne').val();
+    console.log(text);
 }
